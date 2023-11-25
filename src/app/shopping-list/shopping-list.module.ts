484e6a2ac1d3515287bb982e12774273457b6414
Snezana <snezanaanastasova99@gmail.com>
2023-11-25 20:23:04 +0100

    Added angular modules and optimized apps

---
+++ src/app/shopping-list/shopping-list.module.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { ShoppingEditComponent } from "./shopping-edit/shopping-edit.component";
+import { ShoppingListComponent } from "./shopping-list.component";
+import { ShoppingListRoutingModule } from "./shopping-list-routing.module";
+import { SharedModule } from "../shared/shared.module";
+
+@NgModule({
+  declarations: [
+    ShoppingListComponent,
+    ShoppingEditComponent,
+  ],
+  imports: [RouterModule, SharedModule, FormsModule, ShoppingListRoutingModule]
+})
+export class ShoppingListModule {
+
+}
